6513d2271129b77a576dbed12df248d14d7a246c
patpalmerston <patpalmerston@gmail.com>
2019-03-08 09:00:56 -0800

    gotta fix the select method bug

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -19,42 +19,49 @@ class TabLink {
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
       this.cards = document.querySelectorAll(`.card[data-tab='${this.tabElement.dataset.tab}']`);
-      console.log(this.cards);
+      // console.log(this.cards);
     }
     
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map();
+     this.cards = Array.from(this.cards).map(function(card) {
+      return new TabCard(card)});
+      // console.log(this.cards);
 
     // Add a click event that invokes this.selectTab
-    // this.tabElement.addEventListener();
+    this.tabElement.addEventListener('click', selectTab());
   }
 
-  /*
+  
   selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach(function(tab){
+      tab.classList.remove('active-tab')
+    })
+    
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach(function(card) {
+      card.classList.style.display('none');
+    })
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement.classList.add("active-tab");
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
-  */
+  
 }
 
-/*
+
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
@@ -66,7 +73,7 @@ class TabCard {
   }
 
 }
-*/
+
 
 /* START HERE: 
 
